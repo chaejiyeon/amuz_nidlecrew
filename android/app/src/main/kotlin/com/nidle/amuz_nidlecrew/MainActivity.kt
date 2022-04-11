1a59871ea9ae0c8730c0f4dcbe5e1ad3695f35b9
jiyeon <delay@amuz.co.kr>
2022-04-11 10:32:07 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/nidle/amuz_nidlecrew/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nidle.amuz_nidlecrew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main/loadingPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class loadingPage extends StatelessWidget {
+  const loadingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
